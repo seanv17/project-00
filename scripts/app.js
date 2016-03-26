f7ea74f4e1d230f64ad7b1965452f632fcf32f6f
seanvsville <seanvanho@gmail.com>
2016-03-26 14:55:23 -0700

    set up player alerts for winning

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
 
-});
+/*function resetGame() {
+  $('#player1').setAttrs(player1DefaultPosition);
+}*/
 
-function player1Move (keystrokes) {
+
+
+
+function player1Move() {
   $(window).on('keyup', function(e) {
     if (e.keyCode === 81) {
-      if ($('#player1').css("right") >= "5") {
-          $('#player1').animate({left: "+=50px"}, 'fast');
-      } else {
-        alert("player 1 wins!!!");
-      }
+      $('#player1').animate({left: "+=50px"}, 100);
+    }
+    if ( $('#player1').css('left') === "1320px") {
+      alert("player 1 wins");
     }
   });
 }
 
-function player2Move (keystrokes) {
+function player2Move() {
   $(window).on('keyup', function(e) {
     if (e.keyCode === 80) {
-      console.log("You pressed P!");
+      $('#player2').animate({left: "+=50px"}, 100);
+    }
+    if ( $('#player2').css('left') === "1320px") {
+      alert("player 2 wins");
+
     }
   });
 }
+
 
 player1Move();
 player2Move();
+
+});
