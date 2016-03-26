2cf20135b1954cd0bda86393fbc6a5d8085f6025
seanvsville <seanvanho@gmail.com>
2016-03-25 23:07:35 -0700

    got moving piece to slide across screen, need to set function for winner and reset game

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
-
-
 });
+
+function player1Move (keystrokes) {
+  $(window).on('keyup', function(e) {
+    if (e.keyCode === 81) {
+      if ($('#player1').css("right") >= "5") {
+          $('#player1').animate({left: "+=50px"}, 'fast');
+      } else {
+        alert("player 1 wins!!!");
+      }
+    }
+  });
+}
+
+function player2Move (keystrokes) {
+  $(window).on('keyup', function(e) {
+    if (e.keyCode === 80) {
+      console.log("You pressed P!");
+    }
+  });
+}
+
+player1Move();
+player2Move();
