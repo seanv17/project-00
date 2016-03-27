2104895a383b67e5435fa3c00a9fe3e9eebd479b
seanvsville <seanvanho@gmail.com>
2016-03-27 10:36:07 -0700

    added reset button

--- scripts/app.js
+++ scripts/app.js
@@ -2,40 +2,60 @@ $(document).ready(function() {
 
   // ! ---  Pseudo Code Plan --- ! //
 
-  //If goodGuys position is less than racetrack width * .90 & keypress for Q is pressed
+  //If goodGuys position is less than racetrack width * .85 & keypress for Q is pressed
   // move goodGuys by 50px at 100(speed)
   // else if
   // else if goodGuys position width is greater than track, goodGuys win
 
   // Inverse for badGuys
-  // If badGuys position is less than racetrack width * .90 & keypress for P is pressed
+  // If badGuys position is less than racetrack width * .85 & keypress for P is pressed
   // move badGuys by 50px at 100(speed)
   // else if badGuys position width is greater than track, bad Guys win
 
-    $(document).on("keydown", function(e) {
+// Global variables
+
+    var $good = $('#good');
+     var $bad = $('#bad');
+
+  // Event listener for reset button
+
+  $('.btn').on('click', function() {
+    $good.css({
+      top: 190,
+      left: 20,
+      position: 'absolute'
+    });
+    $bad.css({
+      top: 190,
+      left: 20,
+      position: 'absolute'
+    });
+  });
+
+    $(document).on('keydown', function(e) {
 
     // Declare all variables
-             var $goodGuys = $('#goodGuys');
-              var $badGuys = $('#badGuys');
-      var goodGuysPosition = $('#goodGuys').position().left;
-       var badGuysPosition = $('#badGuys').position().left;
+
+          var goodPosition = $('#good').position().left;
+           var badPosition = $('#bad').position().left;
             var $goodTrack = $('#goodTrack').width();
              var $badTrack = $('#badTrack').width();
 
-        if ( (e.keyCode === 81) && (goodGuysPosition <  ($goodTrack * 0.85)) ) {
-          $goodGuys.animate({'left': '+=50px'}, 200);
+
+        if ( (e.keyCode === 81) && (goodPosition <  ($goodTrack * 0.85)) ) {
+          $good.animate({'left': '+=50px'}, 50);
         }
 
-        else if ( (e.keyCode === 81) && (goodGuysPosition > ($goodTrack * 0.85) ) ) {
+        else if ( (e.keyCode === 81) && (goodPosition > ($goodTrack * 0.85) ) ) {
           $(document).off('keydown');
-          alert('Hippy Mabel Wins!!!');
+          alert('Levitating Mabel Wins!!!');
         }
 
-        if ( (e.keyCode === 80) && (badGuysPosition <  ($badTrack * 0.82)) ) {
-          $badGuys.animate({left: '+=50px'}, 200);
+        if ( (e.keyCode === 80) && (badPosition <  ($badTrack * 0.85)) ) {
+          $bad.animate({left: '+=50px'}, 50);
         }
 
-        else if ( (e.keyCode === 80) && (badGuysPosition > ($badTrack * 0.82) ) ) {
+        else if ( (e.keyCode === 80) && (badPosition > ($badTrack * 0.85) ) ) {
           $(document).off('keydown');
           alert ('Torpedo Mabel Wins!!!');
         }
