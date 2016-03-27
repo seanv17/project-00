358b7c2c57e23baac333276be1b914550239c831
seanvsville <seanvanho@gmail.com>
2016-03-27 01:04:57 -0700

    fixed players moving after winner declared, add new player icons, wallerpaper, css styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
 
-/*function resetGame() {
-  $('#player1').setAttrs(player1DefaultPosition);
-}*/
+  // ! ---  Pseudo Code Plan --- ! //
 
+  //If goodGuys position is less than racetrack width * .90 & keypress for Q is pressed
+  // move goodGuys by 50px at 100(speed)
+  // else if
+  // else if goodGuys position width is greater than track, goodGuys win
 
+  // Inverse for badGuys
+  // If badGuys position is less than racetrack width * .90 & keypress for P is pressed
+  // move badGuys by 50px at 100(speed)
+  // else if badGuys position width is greater than track, bad Guys win
 
+    $(document).on("keydown", function(e) {
 
-function player1Move() {
-  $(window).on('keyup', function(e) {
-    if (e.keyCode === 81) {
-      $('#player1').animate({left: "+=50px"}, 100);
-    }
-    if ( $('#player1').css('left') === "1320px") {
-      alert("player 1 wins");
-    }
-  });
-}
+    // Declare all variables
+             var $goodGuys = $('#goodGuys');
+              var $badGuys = $('#badGuys');
+      var goodGuysPosition = $('#goodGuys').position().left;
+       var badGuysPosition = $('#badGuys').position().left;
+            var $goodTrack = $('#goodTrack').width();
+             var $badTrack = $('#badTrack').width();
 
-function player2Move() {
-  $(window).on('keyup', function(e) {
-    if (e.keyCode === 80) {
-      $('#player2').animate({left: "+=50px"}, 100);
-    }
-    if ( $('#player2').css('left') === "1320px") {
-      alert("player 2 wins");
+        if ( (e.keyCode === 81) && (goodGuysPosition <  ($goodTrack * 0.85)) ) {
+          $goodGuys.animate({'left': '+=50px'}, 200);
+        }
 
-    }
-  });
-}
+        else if ( (e.keyCode === 81) && (goodGuysPosition > ($goodTrack * 0.85) ) ) {
+          $(document).off('keydown');
+          alert('Hippy Mabel Wins!!!');
+        }
 
+        if ( (e.keyCode === 80) && (badGuysPosition <  ($badTrack * 0.82)) ) {
+          $badGuys.animate({left: '+=50px'}, 200);
+        }
 
-player1Move();
-player2Move();
-
+        else if ( (e.keyCode === 80) && (badGuysPosition > ($badTrack * 0.82) ) ) {
+          $(document).off('keydown');
+          alert ('Torpedo Mabel Wins!!!');
+        }
+      });
 });
